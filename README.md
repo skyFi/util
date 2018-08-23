b2e36a720140d5fcf688582fcbfd42eb1b788c82
方永乐 <fangyongle@innobuddy.com>
2018-08-23 11:50:39 +0800

    feat: 添加邮箱和手机号验证

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # util
 JavaScript 非常规工具库
+
+## Install
+
+```shell
+npm install leutil
+
+// or
+yarn add leutil
+```
+
+## Api
+
+### isEmail(email: String): Boolean 校验邮箱格式
+
+```javascript
+import { isEmail } from 'leutil';
+
+isEmail('abc@def.com');
+// => true
+
+isEmail('abcdef.com');
+// => false
+```
+
+### isPhoneNum(phone: String): Boolean 国内手机号码格式简单校验
+
+```javascript
+import { isPhoneNum } from 'leutil';
+
+isPhoneNum('18767565433');
+// => true
+isPhoneNum('1876756533');
+// => false
+```
--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 'use strict';
 
-const CSS = require('./src/CSSCore.js');
-const DOM = require('./src/DOMCore.js');
+/**
+ * 校验邮箱格式
+ * @param {string} email 待校验的字符串
+ * @return {boolean} 输入的是邮箱则返回true
+ * */
+export const isEmail = (email) => {
+  const re = /^[\w!#\$%&'\*\+\/\=\?\^`\{\|\}~\-]+(:?\.[\w!#\$%&'\*\+\/\=\?\^`\{\|\}~\-]+)*@(?:[a-z0-9](?:[a-z0-9\-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9\-]*[a-z0-9])?$/i;
+  return email && typeof email === 'string' && re.test(email);
+};
 
-module.exports = {
-  CSS,
-  DOM
+/**
+ * 国内手机号码格式简单校验
+ * @param phone 手机号码
+ * @return {boolean} 是手机号则返回true
+ * */
+export const isPhoneNum = (phone) => {
+  return /^1\d{10}$/.test(phone);
 };
